32bff233131bb309e707ed5b429bf52eea0148a2
Colton Raymond <55898834+craymond3@users.noreply.github.com>
2021-12-17 14:01:46 -0600

    Update test1.c

--- test1.c
+++ test1.c
@@ -6,8 +6,8 @@
 
 // Message struct used for request and replies from the server
 struct message {
-    int opcode;
-    //char msg[BUFFER_SIZE];
+    char opcode[BUFFER_SIZE];
+    char msg[BUFFER_SIZE];
     //todo, add more fields for relevant args   
 };
 
@@ -17,16 +17,18 @@ int main(int argc, char* argv[]) {
     //int rc = MFS_Init("royal-29.cs.wisc.edu", 1000);
     //printf("hello world\n");
     struct message *msg = (struct message*) malloc(sizeof(struct message)); 
-    msg->opcode = 5;
-    //strcpy(msg->msg, "5. Test on Opcode 5");
+    strcpy(msg->opcode, "5");
+    strcpy(msg->msg, "5. Test on Opcode 5");
 
-    printf("Here is msg->opcode %d\n", msg->opcode );
-    //printf("Here is msg->msg %s\n", msg->msg );
+    printf("Here is msg->opcode %s\n", msg->opcode );
+    printf("Here is msg->msg %s\n", msg->msg );
 
     // turn struct into string and print!
+    char bitStr[BUFFER_SIZE];
+    strcpy(bitStr, msg->opcode);
+    strcat(bitStr,  msg->msg);
     char structToString[sizeof(struct message)];
-    memcpy(structToString, (void*) &msg, sizeof(struct message));
-
+    memcpy(structToString, bitStr, sizeof(struct message));
     printf("Here is the final result!: %s\n", structToString);
 
     return 0;
