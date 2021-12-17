d1c7f44a0cef571a024721fc4631da12718a6bdb
lauragarling <garling@wisc.edu>
2021-12-16 18:29:37 -0600

    image file, checkpoint section initialized

--- server.c
+++ server.c
@@ -1,10 +1,80 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include "udp.h"
+#include "mfs.h"
 
 #define BUFFER_SIZE (1000)
 
+int end;
+int image;
+int* pieces;
+
+
+int readImage(char* path) {
+    image = open(path, O_RDWR);
+    printf("image fd is %d\n", image);
+    int* temp = malloc(sizeof(int));
+    int ret = read(image, temp, sizeof(int));
+    end = *temp;
+    printf("read end return is %d\n", ret);
+    pieces = malloc(256 * sizeof(int));
+    read(image, pieces, 256 * sizeof(int));
+
+    // everyone is set up - let's go
+    return 0;
+}
+
+
+int initImage(char* path) {
+
+    // set up checkpoint region
+    end = 257;
+    pieces = malloc(256 * sizeof(int));
+
+    // dump to file
+    image = open(path, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
+    if (image < 0) {
+	printf("unable to create file\n");
+	exit(1);
+    }
+
+
+    int num = end;
+    write(image, &num, sizeof(num));
+    write(image, pieces, 256 * sizeof(int));
+
+    // end, pieces, and file image are all initialized - we're ready to go
+    return 0;
+}
+
+
 // server code
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { 
+    
+
+    if (argc != 3) {
+	printf("usage: server <hostnum> <pathname>\n");
+	return -1;
+    }
+
+    if (access(argv[2], F_OK) == 0) {
+	readImage(argv[2]);
+	printf("reading\n");
+    } else {
+	initImage(argv[2]);
+	printf("initializing\n");
+    }
+
+    printf("end is %d\n", end);
+
+    free(pieces);
+    int res = close(image);
+    printf("close result is %d", res);
+    return 0;
+
     int sd = UDP_Open(10000);
     assert(sd > -1);
     while (1) {
@@ -12,6 +82,19 @@ int main(int argc, char *argv[]) {
 	char message[BUFFER_SIZE];
 	printf("server:: waiting...\n");
 	int rc = UDP_Read(sd, &addr, message, BUFFER_SIZE);
+
+	// based on content of message, do certain operations
+	//    message could be any of the eight from mfs.c
+	//        stat
+	//        lookup
+	//        write
+	//        read
+	//        creat
+	//        unlink (delete)
+	//        shutdown
+	//    do this after the rc is checked (within that "if" down there)
+
+
 	printf("server:: read message [size:%d contents:(%s)]\n", rc, message);
 	if (rc > 0) {
             char reply[BUFFER_SIZE];
