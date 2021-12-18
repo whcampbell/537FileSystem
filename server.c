eeb497043010c4ebf464039baece4887ac5d00cb
lauragarling <garling@wisc.edu>
2021-12-17 19:03:26 -0600

    traversal implemented, methods outlined in server.c

--- server.c
+++ server.c
@@ -14,6 +14,12 @@ typedef struct __Message {
     //todo, add more field for relevant args   
 } Message;
 
+typedef struct inode {
+    int size;
+    int type;
+    int pointers[14];
+}
+
 struct Message* msg;
 
 int end;
@@ -22,6 +28,70 @@ int* pieces;
 
 //do we need a global socket descriptor?
 //int sd;
+
+// returns location of inode
+// so that you can get file offsets
+int traverse(int inum) {
+    int ppoint = pieces[inum % 256];
+    int bucket = inum - (inum % 256);
+    bucket = bucket / 256;
+    lseek(image, ppoint + bucket * 4, SEEK_SET);
+    int* ipoint = 0;
+    read(image, ipoint, sizeof(int));
+    printf("ipoint is %d\n", ipoint);
+    return *ipoint;
+}
+
+
+int mfRead(int inum, char* buffer, int block) {
+
+}
+
+
+int mfWrite(int inum, char* data) {
+//get addrs of old data blocks
+//write new data blocks
+//write inode block with old and new data pointers
+//write piece of map
+//update checkpoint region with new map piece
+//write checkpoint in file
+//send back response
+}
+
+
+int mfCreat(char* name, int pinum, int type) {
+// check if name exists in parent
+// pick new inum
+// add to parent directory
+// send back confirm code
+}
+
+
+int mfLookup(int pinum, char* name) {      
+    int dpoint = traverse(pinum);
+    // iterate through for matching name
+    // send back corresponding inode
+}
+
+
+int mfStat(int inum) {
+    ipointer = traverse(inum);
+//send back stat data from this inode/file
+}
+
+
+int mfUnlink(int pinum, char* name) {
+    int dpoint = traverse(pinum);
+//iterate through directory to find name
+//swap its inode to -1
+//send back reply
+}
+
+
+int mfShutdown() {
+    // force to disk
+    // send back reply
+}
 
 
 int readImage(char* path) {
@@ -41,7 +111,7 @@ int initImage(char* path) {
 
     // set up checkpoint region
     end = 257;
-    pieces = malloc(256 * sizeof(int));
+    pieces = calloc(256 * sizeof(int));
 
     // dump to file
     image = open(path, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
@@ -50,6 +120,14 @@ int initImage(char* path) {
 	exit(1);
     }
 
+    int* root = malloc(128 * sizeof(struct __MFS_DirEnt_t));
+    struct __MFS_DirEnt_t self;
+    struct __MFS_DirEnt_t parent;
+    char* selfName = ".";
+    char* parentName= = "..";
+    strcpy(self.name, selfName);
+    strcpy(parent.name, parentName);
+    //TODO finish initializing root with own inodes, inode block, map piece
 
     int num = end;
     write(image, &num, sizeof(num));
@@ -58,6 +136,25 @@ int initImage(char* path) {
     // end, pieces, and file image are all initialized - we're ready to go
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // server code
@@ -93,6 +190,14 @@ int main(int argc, char *argv[]) {
 	//        unlink (delete)
 	//        shutdown
 	//    do this after the rc is checked (within that "if" down there)
+	
+	int opcode = atoi(message[0]);
+	switch(opcode) {
+	    case 
+	    case 7: 
+		mfShutdown();
+		break;
+	}
 
 
 	printf("server:: read message [size:%d contents:(%s)]\n", rc, message);
